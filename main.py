e3fcb10ff5fb225e374d08f95628ec3979144598
Ayrik Nabirahni <ayriknabirahni01@gmail.com>
2024-07-30 00:13:43 -0700

    V0.1

---
+++ main.py
@@ -0,0 +1,25 @@
+version = 0.1
+
+text_input = input('\n' + ' Enter text!' + ' \n')
+
+
+
+def encryption(text_input):
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    broken_list = []
+    consonants = []
+    for x in text_input:
+        broken_list.append(x)
+
+
+
+    for x in broken_list:
+        if x not in vowels:
+
+
+
+
+
+
+
+encryption(text_input)
